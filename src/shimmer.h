589a17fc7e99111d44fd8180686a8f1b827f9fe2
Charles <charles.rosenbauer@gmail.com>
2022-08-04 23:29:03 -0500

    Getting back into this project a little

--- src/shimmer.h
+++ src/shimmer.h
@@ -4,60 +4,37 @@
 
 #include "stdint.h"
 
+typedef struct{
+	uint8_t* data;
+	uint64_t size, fill, start;
+}ShimmerCircleBuf;
 
-struct shm_dbuf{
-	uint64_t	tag;
-	uint8_t*	bufferA;
-	uint8_t*	bufferB;
+typedef struct{
+	uint8_t* dataA;
+	uint8_t* dataB;
+	uint64_t size;
+	uint8_t  mode;
+}ShimmerDoubleBuf;
+
+typedef struct{
+	uint64_t lock;
+}ShimmerLock;
+
+typedef struct{
+	uint32_t magic;
+	ShimmerCircleBuf* cbufs;
+	ShimmerDoubleBuf* dbufs;
+	ShimmerLock     * locks;
 	
-	uint32_t	size, mode, lock;
-};
-
-struct shm_circ{
-	uint64_t	tag;
-	uint8_t*	buffer;
-	uint32_t	size, head, tail, lock;
-};
-
-struct shm_bkpt{
-	uint64_t	tag;
-	uint8_t		lock;
-};
+	int cct, ccap, act, acap, lct, lcap;
+	ShimmerLock safety;
+	
+	
+}ShimmerContext;
 
 
-/*
- * Context is a struct that gets allocated to address 0xCABBA9E00.
- * It stores a data structure for indexing shmem units.
- * If it is not found, or if it has an invalid magic constant,
- * shimmer initialization fails and returns an error.
- */
-struct shimmer_context{
-	uint64_t	magic;
-	uint32_t	size;
-};
-
-
-/* initialization */
-int init_shimmer  (uint64_t, uint32_t);
-int init_shm_dbuf (uint64_t, uint32_t);
-int init_shm_circ (uint64_t, uint32_t);
-int init_shm_bkpt (uint64_t);
-
-/* double buffer operations */
-int write_shm_dbuf(struct shm_dbuf*, uint32_t, uint32_t, void*);
-int  flip_shm_dbuf(struct shm_dbuf*);
-int  read_shm_dbuf(struct shm_dbuf*, uint32_t, uint32_t, void*);
-int  lock_shm_dbuf(struct shm_dbuf*);
-
-/* circular buffer operations */
-int write_shm_circ(struct shm_circ*, uint32_t, void*);
-int  read_shm_circ(struct shm_circ*, uint32_t, void*);
-int  lock_shm_circ(struct shm_circ*);
-
-/* breakpoint operations */
-int  lock_shm_bkpt(struct shm_bkpt*);
-
-
+int				initShimmer	(int);
+ShimmerContext* getContext	();
 
 
 
